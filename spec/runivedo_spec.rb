d43b987e991eb655cedef38e52531f0dea2ba870
Lucas Clemente <luke.clemente@gmail.com>
2012-11-21 21:23:15 +0100

    protocol

--- spec/runivedo_spec.rb
+++ spec/runivedo_spec.rb
@@ -1,7 +1,12 @@
 require "spec_helper"
 
 describe Runivedo::Runivedo do
+  it "handshakes" do
+    
+  end
+
   context "when running simple queries" do
     let(:connection) { MockConnection.new }
+
   end
 end
