8877b99cbf06e705bb68ee459c307aabccbc7497
Lucas Clemente <luke.clemente@gmail.com>
2013-04-15 13:17:03 +0200

    tests

--- spec/remote_object_spec.rb
+++ spec/remote_object_spec.rb
@@ -10,6 +10,20 @@ class MockStream
   end
 end
 
+class MockMessage
+  def initialize(*data)
+    @data = data
+  end
+
+  def read
+    @data.shift
+  end
+
+  def has_data?
+    @data.count > 0
+  end
+end
+
 describe Runivedo::RemoteObject do
   let(:stream) { MockStream.new }
   let(:connection) { c = double(:connection); c.stub(:register_ro); c.stub(:stream).and_return(stream); c }
@@ -17,26 +31,25 @@ describe Runivedo::RemoteObject do
 
   it 'does rom calls' do
     ro
-    msg = double(:msg)
-    msg.stub(:read).and_return(2, 0, 0, 42)
-    stream.callback = lambda {ro.send(:receive, msg)}
+    stream.callback = lambda {ro.send(:receive, MockMessage.new(2, 0, 0, 42))}
     ro.call_rom('foo').should == 42
     stream.sent_data.should == [2, 1, 0, 'foo']
     # Second call
     stream.sent_data.clear
-    msg = double(:msg)
-    msg.stub(:read).and_return(2, 1, 0, 42)
-    stream.callback = lambda {ro.send(:receive, msg)}
+    stream.callback = lambda {ro.send(:receive, MockMessage.new(2, 1, 0, 42))}
     ro.call_rom('fooBar', 23).should == 42
     stream.sent_data.should == [2, 1, 1, 'fooBar', 23]
   end
 
   it 'returns new remote objects' do
     ro
-    msg = double(:msg)
-    msg.stub(:read).and_return(2, 0, 1, 42, "")
-    stream.callback = lambda {ro.send(:receive, msg)}
+    stream.callback = lambda {ro.send(:receive, MockMessage.new(2, 0, 1, 42, ""))}
     new_ro = ro.call_rom('foo')
     new_ro.id.should == 42
+  end
+
+  it 'receives notifications' do
+    ro.should_receive(:notification).with("foo", 42)
+    ro.send(:receive, MockMessage.new(3, "foo", 42))
   end
 end
