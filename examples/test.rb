0be1a43f6da7fd35d20e1ada4d85e415efc53b91
Lucas Clemente <luke.clemente@gmail.com>
2013-04-28 21:59:21 +0200

    bench

--- examples/test.rb
+++ examples/test.rb
@@ -4,9 +4,8 @@ require "runivedo"
 
 Thread.abort_on_exception=true
 
-runivedo = Runivedo::Connection.new("ws://84.164.176.88:9001/f8018f09-fb75-4d3d-8e11-44b2dc796130")
+runivedo = Runivedo::Connection.new("ws://10.0.0.42:9001/f8018f09-fb75-4d3d-8e11-44b2dc796130")
 query = runivedo.get_perspective("6e5a3a08-9bb0-4d92-ad04-7c6fed3874fa").query
-query.prepare("SELECT * FROM tables")
-query.execute.each do |row|
-  puts row.map(&:to_s).join(', ')
+loop do
+  query.prepare('SELECT * FROM tables').execute.to_a
 end
