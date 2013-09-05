7c4d0176a8922b6cabd0e965586594164fa576d0
Lucas Clemente <luke.clemente@gmail.com>
2013-09-05 17:37:16 +0200

    result uses new notification syntax

--- lib/runivedo/result.rb
+++ lib/runivedo/result.rb
@@ -8,6 +8,14 @@ module Runivedo
       @rows = Queue.new
       @num_rows = Future.new
       @columns = Future.new
+
+      self.on('setResultToField') { |*args| @columns.complete(args.first.map { |f| f[1] }) }
+      self.on('appendTuple') { |*args| @rows << args.first }
+      self.on('setNTuplesAffected') { |*args| @num_rows.complete(args.first) }
+      self.on('setCompleted') { |*args| @rows << nil }
+      self.on('setErrorMessage') { |*args| @rows << RunivedoSqlError.new("error executing query: #{args.first}") }
+      self.on('setNColumns') { |*args| }
+      self.on('tuplesAffected') { |*args| }
     end
 
     def num_affected_rows
@@ -22,25 +30,6 @@ module Runivedo
       while row = @rows.pop
         raise row if row.is_a? Exception
         block.call(row)
-      end
-    end
-
-    private
-
-    def notification(name, *args)
-      case name
-      when 'setResultToField'
-        @columns.complete(args.first.map { |f| f[1] })
-      when 'appendTuple'
-        @rows << args.first
-      when 'setNTuplesAffected'
-        @num_rows.complete(args.first)
-      when 'setCompleted'
-        @rows << nil
-      when 'setErrorMessage'
-        @rows << RunivedoSqlError.new("error executing query: #{args.first}")
-      else
-        puts "received notification #{name}"
       end
     end
   end
