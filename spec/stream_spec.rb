89c503877644ada9c95d04e1e15f6b91163ef37d
Lucas Clemente <lucas@clemente.io>
2013-11-14 13:50:49 +0100

    fix encoding issue

--- spec/stream_spec.rb
+++ spec/stream_spec.rb
@@ -7,26 +7,26 @@ describe Runivedo::Stream do
     it "sends null" do
       message << nil
       # message.buffer.should == "\x00"
-      message.buffer.should == "\xF6"
+      message.buffer.should == "\xF6".b
     end
 
     it "sends bools" do
       message << true
       message << false
       # message.buffer.should == "\x01\x02"
-      message.buffer.should == "\xF5\xF4"
+      message.buffer.should == "\xF5\xF4".b
     end
 
     it "sends integers" do
       message << 42
       # message.buffer.should == "\x12\x2A" + "\x00" * 7
-      message.buffer.should == "\x18\x2a"
+      message.buffer.should == "\x18\x2a".b
     end
 
     it "sends floats" do
       message << 8.0e-323
       # message.buffer.should == "\x15\x10" + "\x00" * 7
-      message.buffer.should == "\xfb\x10" + "\x00" * 7
+      message.buffer.should == "\xfb\x10".b + "\x00".b * 7
     end
 
     it "sends strings" do
@@ -38,43 +38,43 @@ describe Runivedo::Stream do
     it "sends symbols as strings" do
       message << :foobar
       # message.buffer.should == "\x1e\x06\x00\x00\x00foobar"
-      message.buffer.should == "\x66foobar"
+      message.buffer.should == "\x66foobar".b
     end
 
     it "sends arrays" do
       message << %w(foo bar)
       # message.buffer.should == "\x3c\x02\x00\x00\x00\x1e\x03\x00\x00\x00foo\x1e\x03\x00\x00\x00bar"
-      message.buffer.should == "\x82\x63foo\x63bar"
+      message.buffer.should == "\x82\x63foo\x63bar".b
     end
 
     it "sends maps" do
       # message << {"foo" => true, "bar" => false}
       # message.buffer.should == "\x3d\x02\x00\x00\x00\x1e\x03\x00\x00\x00foo\x01\x1e\x03\x00\x00\x00bar\x02"
       message << {"bar" => false, "foo" => true}
-      message.buffer.should == "\xa2bar\x72\xf4foo\xf5"
+      message.buffer.should == "\xa2bar\x72\xf4foo\xf5".b
     end
 
     it 'sends uuids' do
       uuid = UUIDTools::UUID.random_create
       message << uuid
       # message.buffer.should == "\x2a" + uuid.raw
-      message.buffer.should == "\xc7\x50" + uuid.raw
+      message.buffer.should == "\xc7\x50".b + uuid.raw
     end
   end
 
   describe "receiving" do
     it "receives null" do
       # message = Runivedo::Stream::Message.new("\x00")
-      message = Runivedo::Stream::Message.new("\xf6")
+      message = Runivedo::Stream::Message.new("\xf6".b)
       message.read.should == nil
     end
 
     it "receives bools" do
       # message = Runivedo::Stream::Message.new("\x01")
-      message = Runivedo::Stream::Message.new("\xf5")
+      message = Runivedo::Stream::Message.new("\xf5".b)
       message.read.should == true
       #message = Runivedo::Stream::Message.new("\x02")
-      message = Runivedo::Stream::Message.new("\x02")
+      message = Runivedo::Stream::Message.new("\x02".b)
       message.read.should == false
     end
 
@@ -83,16 +83,16 @@ describe Runivedo::Stream do
       #   message = Runivedo::Stream::Message.new([i].pack("C") + "\x2A" + "\x00" * (s-1))
       #   message.read.should == 42
       # end
-      message = Runivedo::Stream::Message.new("\x18\x2a")
+      message = Runivedo::Stream::Message.new("\x18\x2a".b)
       message.read.should == 42
     end
 
     it "receives floats" do
       # message = Runivedo::Stream::Message.new("\x14\x10" + "\x00" * 3)
-      message = Runivedo::Stream::Message.new("\xfa\x10" + "\x00" * 3)
+      message = Runivedo::Stream::Message.new("\xfa\x10".b + "\x00".b * 3)
       message.read.should == 2.2420775429197073e-44
       # message = Runivedo::Stream::Message.new("\x15\x10" + "\x00" * 7)
-      message = Runivedo::Stream::Message.new("\xfb\x10" + "\x00" * 7)
+      message = Runivedo::Stream::Message.new("\xfb\x10".b + "\x00".b * 7)
       message.read.should == 8.0e-323
     end
 
@@ -104,13 +104,13 @@ describe Runivedo::Stream do
       #   "\x08" "\x2A\x00\x00\x00\x00\x00\x00\x00" "\x01"
       #   )
       # 4.times {message.read.should == BigDecimal.new("4.2")}
-      message = Runivedo::Stream::Message.new("\xc4\x82\x18\x2a\x20")
+      message = Runivedo::Stream::Message.new("\xc4\x82\x18\x2a\x20".b)
       message.read.should == BigDecimal.new("4.2")
     end
 
     it "receives strings" do
       # message = Runivedo::Stream::Message.new("\x1e\x06\x00\x00\x00foobar")
-      message = Runivedo::Stream::Message.new("\x66foobar")
+      message = Runivedo::Stream::Message.new("\x66foobar".b)
       message.read.should == "foobar"
       # message = Runivedo::Stream::Message.new("\x1f\x06\x00\x00\x00f\x00o\x00o\x00b\x00a\x00r\x00")
       # message.read.should == "foobar"
@@ -118,14 +118,14 @@ describe Runivedo::Stream do
 
     it "receives arrays" do
       # message = Runivedo::Stream::Message.new("\x3c\x02\x00\x00\x00\x1e\x03\x00\x00\x00foo\x1e\x03\x00\x00\x00bar")
-      message = Runivedo::Stream::Message.new("\x82\x63foo\x63bar")
+      message = Runivedo::Stream::Message.new("\x82\x63foo\x63bar".b)
       message.read.should == %w(foo bar)
     end
 
     it "receives maps" do
       # message = Runivedo::Stream::Message.new("\x3d\x02\x00\x00\x00\x1e\x03\x00\x00\x00foo\x0a\x01\x1e\x03\x00\x00\x00bar\x0a\x02")
       # message.read.should == {"foo" => 1, "bar" => 2}
-      message = Runivedo::Stream::Message.new("\xa2bar\x72\xf4foo\xf5")
+      message = Runivedo::Stream::Message.new("\xa2bar\x72\xf4foo\xf5".b)
       message.read.should == {"bar" => 2, "foo" => 1}
     end
 
@@ -136,13 +136,13 @@ describe Runivedo::Stream do
 
     it 'receives datetimes' do
       # message = Runivedo::Stream::Message.new("\x33\x40\x7F\xFF\x0D\x8B\xDA\x04\x00")
-      message = Runivedo::Stream::Message.new("\xc9\x1a\x59\x6e\x6a\x55")
+      message = Runivedo::Stream::Message.new("\xc9\x1a\x59\x6e\x6a\x55".b)
       message.read.should == Time.at(1366190677)
     end
 
     it 'receives times' do
       # message = Runivedo::Stream::Message.new("\x34\x40\x7F\xFF\x0D\x8B\xDA\x04\x00")
-      message = Runivedo::Stream::Message.new("\xc8\x1a\x59\x6e\x6a\x55")
+      message = Runivedo::Stream::Message.new("\xc8\x1a\x59\x6e\x6a\x55".b)
       message.read.should == Time.at(1366190677)
     end
 
@@ -208,7 +208,7 @@ describe Runivedo::Stream do
   describe Runivedo::VariantStream do
     it "receives" do
       # s = Runivedo::VariantStream.new(StringIO.new("\x0D\x2A\x00\x00\x00\x00\x00\x00\x00\x1e\x06\x00\x00\x00foobar"))
-      s = Runivedo::VariantStream.new(StringIO.new("\x18\x2a\x66foobar"))
+      s = Runivedo::VariantStream.new(StringIO.new("\x18\x2a\x66foobar".b))
       s.read.should == 42
       s.read.should == "foobar"
       s.has_data?.should be_false
