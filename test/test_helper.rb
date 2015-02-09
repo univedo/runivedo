7740da4744758b8fda9da1ef7da1a285ecf6822c
Lucas Clemente <lucas@clemente.io>
2015-02-09 16:07:21 +0100

    fix tests for 0.5.0

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,6 @@ require "minitest/autorun"
 require "minitest/emoji"
 require "runivedo"
 
-TEST_URL = "ws://localhost:9011/"
+TEST_URL = "ws://localhost:9011"
 TEST_BUCKET = "cbd79ca3-1dd4-4ddf-a964-618b593d5fdb"
-TEST_AUTH = {}
+TEST_AUTH = {username: "marvin"}
